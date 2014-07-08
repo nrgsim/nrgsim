42a2bf26879d4a8021b8bb41ccfc02ee41789ddf
spaquin <stevepaquin@tryonsoft.com>
2014-07-08 07:48:39 -0700

    Fixed an issue with creating a new user.

--- public/app/js/services/user_service.js
+++ public/app/js/services/user_service.js
@@ -1,7 +1,7 @@
 window.app.services.UserService = {
 
-  create: function(id, name, email, password, roles, success, failure) {
-    var user = { id: id, name: name, email: email, password: password, provider: 'local', roles: roles };
+  create: function(name, email, password, roles, success, failure) {
+    var user = { name: name, email: email, password: password, provider: 'local', roles: roles };
     $.ajax({
       type: "POST",
       url: '/user',
