ef8f329b8e8584429415cd6287f015e2b9d58f38
Steve Paquin <stevepaquin@tryonsoft.com>
2014-07-16 13:42:40 -0700

    Refactored the run simulation python script

--- scripts/runsimulation.py
+++ scripts/runsimulation.py
@@ -3,7 +3,13 @@ import os
 import json
 import csv
 
-def convertDataToCSV(jsonData):
+def createSimulationDirectory(simulationid):
+  directory = 'simulations/{0}'.format(simulationid)
+  if not os.path.exists(directory):
+      os.makedirs(directory)
+  return directory
+
+def convertDataToCSV(jsondata):
   # This would take the json object passed in and convert it to CSV data.
   # The format of the data is specified in public/transport.json
   csvdata = []
@@ -12,7 +18,18 @@ def convertDataToCSV(jsonData):
   csvdata.append([jsondata["zone"][0]["vertices"][0]["x"], jsondata["zone"][0]["vertices"][0]["y"], jsondata["zone"][0]["vertices"][0]["z"]])
   return csvdata
 
+def createParametersFile(directory, jsondata):
+  outputfile = directory + '/parameters.csv'
+  with open(outputfile, 'w', newline='') as csvfile:
+      csvfile = csv.writer(csvfile, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
+      csvfile.writerows(convertDataToCSV(jsondata))
 
+def runSimulation(simulationid, jsondata):
+  directory = createSimulationDirectory(simulationid)
+  createParametersFile(directory, jsondata)
+
+runSimulation(sys.argv[1], json.JSONDecoder().decode(sys.argv[2]))
+'''
 print(sys.argv[1])
 #print(sys.argv[2])
 
@@ -28,3 +45,4 @@ if not os.path.exists(directory):
 with open(outputfile, 'w', newline='') as csvfile:
     csvfile = csv.writer(csvfile, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
     csvfile.writerows(convertDataToCSV(jsondata))
+'''
