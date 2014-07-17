79596f3d7d578e0cfd82d8239ad70f97bfcc1eec
Steve Paquin <stevepaquin@tryonsoft.com>
2014-07-17 12:15:14 -0700

    More python script updates.
    Copy files and run a simulation.

--- scripts/runsimulation.py
+++ scripts/runsimulation.py
@@ -2,6 +2,8 @@ import sys
 import os
 import json
 import csv
+import shutil
+import subprocess
 
 def createSimulationDirectory(simulationid):
   directory = 'simulations/{0}'.format(simulationid)
@@ -22,14 +24,25 @@ def createParametersFile(directory, jsondata):
       csvfile = csv.writer(csvfile, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
       csvfile.writerows(convertDataToCSV(jsondata))
 
+def copySupportingFiles(simulationDirectory, jsondata):
+  # TODO: check jsondata.weatherFile to determin which weather file to copy
+  shutil.copyfile('../weather/test.epw', simulationDirectory + os.sep + 'in.epw')
+  #TODO: I think in version 1 we will always use the same idf file but it will get it's data from the parameter file.
+  #      We need to replace the one that is currently there with the correct one that takes a parameter file.
+  shutil.copyfile('../idf/Geometry.idf', simulationDirectory + os.sep + 'in.idf')
+
+def executeSimulation(simulationDirectory, resultsDirectory):
+  subprocess.call(['java', '-jar', '../jess_client/JESS_Client.jar', '-cfg', '../jess_client/client.cfg', '-job', simulationDirectory, '-type', 'STD_SINGLE_JOB', '-output', resultsDirectory])
+
 def runSimulation(simulationid, jsondata):
   directory = createSimulationDirectory(simulationid)
+  resultsDirectory = directory + os.sep + 'output';
 
   createParametersFile(directory, jsondata)
-  # TODO: copy a weather file and idf file to the directory and run the simulation
+  copySupportingFiles(directory, jsondata)
+  executeSimulation(directory, resultsDirectory)
 
   # Send the directory that the simulation results are in back to the caller.
-  resultsDirectory = directory + os.sep + 'output';
   print(resultsDirectory)
 
 runSimulation(sys.argv[1], json.JSONDecoder().decode(sys.argv[2]))
