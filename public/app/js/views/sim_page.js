6ea846606adb6a7312faf4ec19a3a7a25753bcef
Steve Paquin <stevepaquin@tryonsoft.com>
2014-11-24 13:39:36 -0800

    Fixed some compiler errors

--- public/app/js/views/sim_page.js
+++ public/app/js/views/sim_page.js
@@ -261,7 +261,7 @@ sz2=str(CDbl(BuildingHeight)-windowheadersize-Southwh)
    // $("#hotwater-rate").slider({ min: 0, max: 30, value: 11 });
 
     $("#Width").slider({ min: 3, max: 21.336, value: 4.572, slide: self.updateWidth });
-    $("#Height").slider({ min: 2.1336, max: 8, value: 2.4511, slide: self.updateHeight });  
+    $("#Height").slider({ min: 2.1336, max: 8, value: 2.4511, slide: self.updateHeight });
     //depth is not needed for facade model and so default to 15ft
     $("#Depth").slider({ min: 4.572, max: 4.572, value: 4.572, slide: self.updateDepth });
    
@@ -274,15 +274,15 @@ sz2=str(CDbl(BuildingHeight)-windowheadersize-Southwh)
 //could we remove the checkbox for window with an "if WinGR>1 then yes" statement? 
     $("#WinGR").slider({ min: 1, max: 99, value: 40, disabled: true, slide: self.updateSliderDisplay });
     $("#insulation-level").slider({ min: 0.1, max: 10, value: 3, slide: self.updateSliderDisplay });
-   $("#Window_U_Value").slider({ min: 1.94, max: 5.8, value: 3.12, slide: self.updateSliderDisplay });
-   $("#Window_SHGC").slider({ min: 0.25, max: 1, value: 0.42, slide: self.updateSliderDisplay });
-   $("#WinOverhangR").slider({ min: 0.1, max: 1, value: 0.4, slide: self.updateSliderDisplay });
-   $("#WinFinR").slider({ min: 0.1, max: 1, value: 0.4, slide: self.updateSliderDisplay })
+    $("#Window_U_Value").slider({ min: 1.94, max: 5.8, value: 3.12, slide: self.updateSliderDisplay });
+    $("#Window_SHGC").slider({ min: 0.25, max: 1, value: 0.42, slide: self.updateSliderDisplay });
+    $("#WinOverhangR").slider({ min: 0.1, max: 1, value: 0.4, slide: self.updateSliderDisplay });
+    $("#WinFinR").slider({ min: 0.1, max: 1, value: 0.4, slide: self.updateSliderDisplay });
 
-   $("#CoolingSP").slider({ min: 23, max: 30, value: 25, slide: self.updateSliderDisplay })
-   //SB must be higher than SP or error $("#CoolingSB").slider({ min: 23, max: 40, value: 27, slide: self.updateSliderDisplay })
-   $("#HeatingSP").slider({ min: 18, max: 22, value: 21, slide: self.updateSliderDisplay })
-   //SB must be higher than SP or error $("#HeatingSB").slider({ min: 12, max: 20, value: 18, slide: self.updateSliderDisplay })
+    $("#CoolingSP").slider({ min: 23, max: 30, value: 25, slide: self.updateSliderDisplay });
+    //SB must be higher than SP or error $("#CoolingSB").slider({ min: 23, max: 40, value: 27, slide: self.updateSliderDisplay })
+    $("#HeatingSP").slider({ min: 18, max: 22, value: 21, slide: self.updateSliderDisplay });
+    //SB must be higher than SP or error $("#HeatingSB").slider({ min: 12, max: 20, value: 18, slide: self.updateSliderDisplay })
 
 
    $("#run-button").button();
