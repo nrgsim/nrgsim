fe21f6e099da0af6f66a53022546ebde8a695a0f
nrgsim <jeremiah@nrgsim.com>
2014-11-25 21:44:20 -0800

    Updated the BioPCM sliders to be vertacal again

--- public/app/js/views/sim_page.js
+++ public/app/js/views/sim_page.js
@@ -276,8 +276,8 @@ sz2=str(CDbl(BuildingHeight)-windowheadersize-Southwh)
     $("#insulation-level").slider({ min: 0.1, max: 10, value: 3, slide: self.updateSliderDisplay });
     $("#Window_U_Value").slider({ min: 1.94, max: 5.8, value: 3.12, step: 0.1, slide: self.updateSliderDisplay });
     $("#Window_SHGC").slider({ min: 0.25, max: 1, value: 0.42, step: 0.1, slide: self.updateSliderDisplay });
-    $("#WinOverhangR").slider({ min: 0.1, max: 1, value: 0.2, slide: self.updateSliderDisplay });
-    $("#WinFinR").slider({ min: 0.1, max: 1, value: 0.2, slide: self.updateSliderDisplay });
+    $("#WinOverhangR").slider({ min: 0.1, max: 1, value: 0.2, step: 0.1, slide: self.updateSliderDisplay });
+    $("#WinFinR").slider({ min: 0.1, max: 1, value: 0.2, step: 0.1, slide: self.updateSliderDisplay });
 
     $("#CoolingSP").slider({ min: 23, max: 30, value: 25, step: 1, slide: self.updateSliderDisplay });
     //SB must be higher than SP or error $("#CoolingSB").slider({ min: 23, max: 40, value: 27, slide: self.updateSliderDisplay })
@@ -286,8 +286,8 @@ sz2=str(CDbl(BuildingHeight)-windowheadersize-Southwh)
     
     //$('#mvalue').slider({ min: 27, max: 91, step: 2, value: 51, slide: self.updateSliderDisplay });
     //$('#qvalue').slider({ min: 23, max: 25, value: 27, step: 2, slide: self.updateSliderDisplay });
-    $('#mvalue').slider({ min: 27, max: 91, step: 2, value: 51, orientation: 'vertical' });
-    $('#qvalue').slider({ min: 23, max: 25, step: 2, value: 27, orientation: 'vertical' });
+    $('#mvalue').slider({ min: 27, max: 91, step: 2, value: 51, orientation: 'vertical'});
+    $('#qvalue').slider({ min: 23, max: 25, step: 2, value: 27, orientation: 'vertical'});
 
 
    $("#run-button").button();
